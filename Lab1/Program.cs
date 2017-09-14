e64e4f41c16916accacd8bf32c9e167d62a83855
Victoria Machen <victoriamachen@gmail.com>
2017-09-14 12:20:58 -0500

    importing project

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //problem 1: display name to the user.
+            Console.WriteLine("Name: Tori");
+            Console.WriteLine();
+
+            //problem 2: display nemesis' name
+            Console.WriteLine("Nemesis: Chris");
+            Console.WriteLine();
+
+            //problem 3: display favorite three games
+            Console.WriteLine("Terraria");
+            Console.WriteLine("Hollow Knight");
+            Console.WriteLine("Stardew Valley\n");
+        }
+    }
+}
